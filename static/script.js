a25044e71ea378b5d9e6d1592da420458c2a3bb1
WeronikaDebowska <weronika.debowska@gmail.com>
2018-08-07 19:32:01 +0200

    activation and disactivation of rows

--- static/script.js
+++ static/script.js
@@ -1,3 +1,6 @@
+var numberOfFilledHoles = 0;
+ActivateRow(1)
+
 function allowDrop(ev) {
     ev.preventDefault();
 }
@@ -8,6 +11,53 @@ function drag(ev) {
 
 function drop(ev) {
     ev.preventDefault();
-    var data = ev.dataTransfer.getData("text");
-    ev.target.appendChild(document.getElementById(data));
+    let data = ev.dataTransfer.getData("text"); //dragged pegs
+    let guess = ev.target;                      //state after peg drop
+    guess.style.color = data.toString();
+    guess.className = "fas fa-circle fa-3x";
+    numberOfFilledHoles += 1;
+    checkStatus()
+}
+
+function checkStatus() {
+    if (numberOfFilledHoles % 4 == 0){
+        alert('time for chsanging a row')
+        let numberOfRowCompleted = numberOfFilledHoles / 4;
+        console.log(numberOfRowCompleted)
+        let numberOfRowToActivate = numberOfRowCompleted + 1;
+        console.log(numberOfRowToActivate)
+        ActivateRow(numberOfRowToActivate);
+        DesactivateRow(numberOfRowCompleted);
+
+    }
+}
+
+function checkIfEnd() {
+    checkIfGuessed();
+}
+
+function ActivateRow(Number) {
+    let RowToActivate = document.getElementById(Number);
+    let holesToActivate = RowToActivate.children;
+    let ArrayOfHoles = Array.from(holesToActivate);
+    ArrayOfHoles.forEach( function (hole){
+        let circles = Array.from(hole.children);
+        circles.forEach( function(circle) {
+            circle.setAttribute("ondrop", "drop(event)");
+            circle.setAttribute("ondragover", "allowDrop(event)");
+        })
+    })
+}
+
+function DesactivateRow(Number) {
+    let RowToActivate = document.getElementById(Number);
+    let holesToActivate = RowToActivate.children;
+    let ArrayOfHoles = Array.from(holesToActivate);
+    ArrayOfHoles.forEach( function (hole){
+        let circles = Array.from(hole.children);
+        circles.forEach( function(circle) {
+            circle.setAttribute("ondrop", "");
+            circle.setAttribute("ondragover", "");
+        })
+    })
 }
